b5eb5cb8aec877529e656c9d845f5cbfe5ebf1c1
churchill <churchillotiende@gmail.com>
2022-09-04 18:33:09 +0300

    chat commit

---
+++ src/pages/chat/[id].js
@@ -0,0 +1,66 @@
+import React from 'react'
+import Sidebar from '../../components/LiveChat/Sidebar'
+import Chat from '../../components/LiveChat/Chat'
+import { BoxChat, ChatBody, ChatComp, Section, SectionTitle, SidebarChat, SidebarComp } from '../../styles/GlobalComponents';
+import { db } from '../../firebase';
+
+const Chat1 = ({chat , messages}) => {
+  return (
+    <>
+    {/* <Section id = 'chat'> */}
+      <SectionTitle>Live Chat </SectionTitle>
+      {/* <BoxChat> */}
+        {/* <SidebarChat> */}
+          <ChatBody>
+              <SidebarComp>
+                  <Sidebar />
+              </SidebarComp>
+              <ChatComp>
+                      <Chat chat = {chat} messages = {messages}/>
+              </ChatComp>
+          </ChatBody>
+        {/* </SidebarChat> */}
+      {/* </BoxChat> */}
+    {/* </Section> */}
+  </>
+  )
+}
+
+export default Chat1;
+
+export async function getServerSideProps(context){
+
+  const ref = db.collection('chats').doc(context.query.id)
+
+  // prep the messages on the server
+
+  let messages = null;
+  const messagesRes = await ref
+    .collection('messages')
+    .orderBy('timestamp' , 'asc')
+    .get();
+
+  messages = messagesRes.docs.map(doc => ({
+    id:doc.id,
+    ...doc.data(),
+  })).map(messages => ({
+    ...messages,
+    timestamp:messages.timestamp.toDate().getTime()
+  }))
+
+// Prep the chat
+
+const chatRes = await ref.get();
+let chat =null
+chat = {
+  id:chatRes.id,
+  ...chatRes.data()
+}
+
+return {
+  props:{
+    messages:JSON.stringify(messages),
+    chat:chat
+  }
+}
+}
